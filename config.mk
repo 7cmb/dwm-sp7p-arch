33fe51293adc316a7f345528a4a4723d4a81fecb
7cmb <jiezhao.Z@outlook.com>
2023-11-10 16:10:18 +0800

    INIT REPO

--- config.mk
+++ config.mk
@@ -7,6 +7,8 @@ VERSION = 6.4
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
+##X11INC = /usr/include/X11
+##X11LIB = /usr/lib/X11
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
@@ -23,7 +25,7 @@ FREETYPEINC = /usr/include/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
